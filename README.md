1874647089b81e2786354d5e8287affae5e46b47
tosin1691 <119135079+tosin1691@users.noreply.github.com>
2023-12-11 19:43:02 +0000

    updated readMe file and made final changes to JS and CSS files.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Work-Day-Schedule
 A calendar application that enables users to save events for each hour of the day. This app runs in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+
+## JQuery
+
+I used HTML, CSS and JQuery to create a calendar app that:
+
+1) Displays the current day at the top of the calender when a user opens the planner.
+2) Presents timeblocks for standard business hours when the user scrolls down.
+3) Color-codes each timeblock based on past, present, and future when the timeblock is viewed.
+4) Allows a user to enter an event when they click a timeblock
+5) Enable users to save the event in local storage when the save button is clicked in that timeblock.
+6) Persists events between refreshes of a page
+
+
+## Installation
+
+N/A
+
+## Usage
+
+N/A 
+
+## Credits
+
+https://stackoverflow.com/ - Utilised resources from this platform and youtube
+
+
+## Screenshot
+![Alt text](<./images/Work-Day-Schedule-app.png>)
+
+## Deployed application
+
+https://tosin1691.github.io/Work-Day-Schedule
+
+
+## License
+
+Please refer to the LICENSE in the repo.
--- starter/index.js
+++ starter/index.js
@@ -1,18 +1,23 @@
+// Get references to the relevant elements
 let currentDayEl = $("#currentDay")
 let containerEl = $(".container")
 
 
-
+//below code utilises dayjs() to display the current day/date at the top of the calender
 dayjs.extend(window.dayjs_plugin_advancedFormat)
 dayjs.extend(window.dayjs_plugin_customParseFormat)
 let today = dayjs();
-let todaysDate = today.format("Do MMMM YYYY")
+let todaysDate = today.format("dddd, Do MMMM YYYY")
 currentDayEl.text(todaysDate)
 
+//below is a varable to set the current time
 let currentTime = dayjs().format("hA")
 
+// below is an Array of 9 empty strings stored in the taskItems variable 
 let taskItems = ["", "", "", "", "", "", "", "", ""]
 
+
+// below pull saved information from local storage
  if (localStorage.getItem("taskItems")){
     let initialTaskArray = JSON.parse(localStorage.getItem("taskItems"))
     if (Array.isArray(initialTaskArray)){
@@ -22,53 +27,48 @@ let taskItems = ["", "", "", "", "", "", "", "", ""]
 
 
  
-
- taskItems.forEach(function(element, index) {
+// forEach method to loop through each array in taskItems
+taskItems.forEach(function(element, index) {
     
+    // created required elements for each element in the taskItems array.
     let timeBlockDiv = $("<div>")
     let timeBlockHour = $("<div>")
     let timeBlockText = $("<textarea>")
     let timeBlockButton = $("<button>")
 
+     // added class and id attributes to the elements created above.
     timeBlockDiv.attr("class", "time-block row")
     timeBlockHour.attr("class", "col-md-1 hour")
     timeBlockText.attr("class", "col-md-10 description")
     timeBlockText.attr("id", "time-block" + index)
     timeBlockButton.attr("class", "btn saveBtn col-md-1")
-
     
+    //below sets start time for the day at 9am and adds hour using the array index 
     let startTime = dayjs().set("hour", 9).add(index, "hour").format("hA")
     
-
+    // adds text to the elements created above
     timeBlockHour.text(startTime)
     timeBlockText.text(element)
 
-
+    //append elements so they display in the browser
     timeBlockDiv.append(timeBlockHour)
     timeBlockDiv.append(timeBlockText)
     timeBlockDiv.append(timeBlockButton)
     containerEl.append(timeBlockDiv)
 
-
-
-    let scheduleTime = dayjs(startTime, "hA")
+    // below was used to convert startTime and currentTime string to parse a time
+    let businessHour = dayjs(startTime, "hA")
     let currentScheduleTime = dayjs(currentTime, "hA")
     
-        if(currentScheduleTime.isBefore(scheduleTime)){
-            timeBlockText.addClass("future")
-            timeBlockText.removeClass("past")
-            timeBlockText.removeClass("present")
-        } else if (currentScheduleTime.isAfter(scheduleTime)){
-            timeBlockText.addClass("past")
-            timeBlockText.removeClass("future")
-            timeBlockText.removeClass("present")
-        } else {
-            timeBlockText.addClass("present")
-            timeBlockText.removeClass("future")
-            timeBlockText.removeClass("past")
-        }
+    if(currentScheduleTime.isBefore(businessHour)){
+        timeBlockText.addClass("future")
+    } else if (currentScheduleTime.isAfter(businessHour)){
+        timeBlockText.addClass("past")
+    } else {
+        timeBlockText.addClass("present")
+    }
 
-
+    // added click event to save value in element to local storage
     timeBlockButton.on("click", function(event){
     
     let task = timeBlockText.val()
@@ -79,15 +79,5 @@ let taskItems = ["", "", "", "", "", "", "", "", ""]
 
 
      })
-
-
-
-    
-    // 
-
-    
-    
-    // 
-   
     
 })
